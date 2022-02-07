47199004a082385812ff37906f087d0b80c0c518
LunashaGit <lunamuylkens@gmail.com>
2022-02-07 21:20:39 +0100

    V1

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ include 'head.php';
 include 'navbar.php';
 ?>
 <img class="index-img" src="img/sass.svg">
-<p class="index-p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint blanditiis ipsam quibusdam rem id ullam, est doloribus facilis enim eligendi eaque debitis rerum quas iste consectetur iure quaerat non nemo.</p>
+<p id="index-p1" class="index-p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint blanditiis ipsam quibusdam rem id ullam, est doloribus facilis enim eligendi eaque debitis rerum quas iste consectetur iure quaerat non nemo.</p>
+<p id="index-p2" class="index-p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint blanditiis ipsam quibusdam rem id ullam, est doloribus facilis enim eligendi eaque debitis rerum quas iste consectetur iure quaerat non nemo.</p>
+<p id="index-p3" class="index-p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint blanditiis ipsam quibusdam rem id ullam, est doloribus facilis enim eligendi eaque debitis rerum quas iste consectetur iure quaerat non nemo.</p>
+
 <nav>
         <ul class="navbar">
             <li><a class="btn1" href="#">btn1</a></li>
